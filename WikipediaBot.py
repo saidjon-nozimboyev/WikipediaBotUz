9a9fde66ed37b338e0101ca37b7bc798af4e823c
saidjon-nozimboyev <146335346+saidjon-nozimboyev@users.noreply.github.com>
2023-11-27 20:17:51 +0500

    main.py

---
+++ WikipediaBot.py
@@ -0,0 +1,31 @@
+import logging
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6437218316:AAE71U1aIc9E677laxoSUEEyIItRfb2AaAk'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+wikipedia.set_lang("uz")
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nThis bot returns whatever you send.")
+
+@dp.message_handler()
+async def WikiInfo(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bunday ma'lumot topilmadi!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
